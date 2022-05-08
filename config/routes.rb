3b938397f510b4fa7c032d951a34ab7ee1a30aad
EC2 Default User <ec2-user@ip-172-31-25-50.ec2.internal>
2022-05-08 13:42:44 +0000

    [task5]

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   	get 'followers' => 'relationships#followers', as: 'followers'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "search" => "searches#search"
+  get '/search', to: 'searches#search'
 end
 
